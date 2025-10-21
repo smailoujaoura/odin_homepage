e9326bd2bd73ca53c9864c3d11fa6cde1cfd4481
Smail Oujaoura <soujaour@e2r11p3.1337.ma>
2025-10-21 18:03:22 +0100

    modify vite config to build in dist folder

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
+import path from 'path'
+
 
 export default defineConfig({
 	root: 'src',
+	build: {
+		outDir: path.resolve(__dirname, 'dist'),
+		emptyOutDir: true,
+	},
 	test: {
 		environment: 'jsdom',
 	},
